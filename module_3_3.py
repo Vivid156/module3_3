951e087177776c1fe9f0ff84f079f67b7ef12427
Vivid156 <167762620+Vivid156@users.noreply.github.com>
2024-07-20 17:41:24 +0300

    Add files via upload

---
+++ module_3_3.py
@@ -0,0 +1,11 @@
+def print_params(a = 1, b = 'строка', c = True):
+    print(a,b,c)
+print_params(b=25)
+print_params(c=[1,2,3])
+values_list = [1, 'фрукт', False]
+values_dict = {'a': 5, 'b':True , 'c': 'word'}
+print_params(*values_list)
+print_params(**values_dict)
+values_list_2 = [54.32, 'строка']
+print_params(*values_list_2, 42)
+
